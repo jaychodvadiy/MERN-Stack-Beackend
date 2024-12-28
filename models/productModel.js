8473cb2b8d717161fa6d8b905c59708a964bfbc6
jaychodvadiy <jaychodvadiya130@gmail.com>
2024-12-29 02:11:51 +0530

    all flie changing and node module add and page cartering  mongodb  connenting

--- models/productModel.js
+++ models/productModel.js
@@ -1,12 +1,65 @@
+const mongoose = require("mongoose"); // Erase if already required
 
-const mongoose = require("mongoose");
+// Declare the Schema of the Mongo model
+var productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    sold: {
+      type: Number,
+      default: 0,
+    },
+    images: [
+      {
+        public_id: String,
+        url: String,
+      },
+    ],
+    color: [{ type: mongoose.Schema.Types.ObjectId, ref: "Color" }],
+    tags: String,
+    ratings: [
+      {
+        star: Number,
+        comment: String,
+        postedby: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+      },
+    ],
+    totalrating: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
 
-const ProductSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  price: { type: Number, required: true },
-  description: { type: String },
-  category: { type: String },
-  image: { type: String },
-});
-
-module.exports = mongoose.model("Product", ProductSchema);
+//Export the model
+module.exports = mongoose.model("Product", productSchema);
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,25 @@
-
-const express = require('express');
-const { getProducts, getProductById, addProduct } = require('../controllers/productController');
+const express = require("express");
+const {
+  createProduct,
+  getaProduct,
+  getAllProduct,
+  updateProduct,
+  deleteProduct,
+  addToWishlist,
+  rating,
+} = require("../controller/productCtrl");
+const { isAdmin, authMiddleware } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
-router.get('/', getProducts);
-router.get('/:id', getProductById);
-router.post('/', addProduct);
+router.post("/", authMiddleware, isAdmin, createProduct);
+
+router.get("/:id", getaProduct);
+router.put("/wishlist", authMiddleware, addToWishlist);
+router.put("/rating", authMiddleware, rating);
+
+router.put("/:id", authMiddleware, isAdmin, updateProduct);
+router.delete("/:id", authMiddleware, isAdmin, deleteProduct);
+
+router.get("/", getAllProduct);
 
 module.exports = router;
